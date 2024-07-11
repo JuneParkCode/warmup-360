39e026c84659165d88a9b943656cacc2a84db01f
Photogrammer <khsj15@khu.ac.kr>
2024-07-11 22:54:55 +0900

    init: SpringBoot Project.

---
+++ src/main/java/org/kernel360/precourse/PrecourseApplication.java
@@ -0,0 +1,13 @@
+package org.kernel360.precourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrecourseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PrecourseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/kernel360/precourse/PrecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kernel360.precourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrecourseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
