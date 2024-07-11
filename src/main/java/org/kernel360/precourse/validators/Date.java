c7a6402b15de7c1d502a34648f2f82d2309f87fb
Photogrammer <khsj15@khu.ac.kr>
2024-07-12 00:45:06 +0900

    feat: Date, DateValidator
    - 날짜 형식을 검증하는 Date, DateValidator 구현

---
+++ src/main/java/org/kernel360/precourse/validators/Date.java
@@ -0,0 +1,21 @@
+package org.kernel360.precourse.validators;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import jakarta.validation.Constraint;
+
+@Constraint(validatedBy = DateValidator.class)
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Date {
+	String pattern() default "yyyyMMdd";
+
+	String message() default "잘못된 형식의 날짜입니다.";
+
+	Class<?>[] groups() default {};
+
+	Class<?>[] payload() default {};
+}
---
+++ src/main/java/org/kernel360/precourse/validators/DateValidator.java
@@ -0,0 +1,31 @@
+package org.kernel360.precourse.validators;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class DateValidator implements ConstraintValidator<Date, String> {
+
+	private String pattern;
+
+	@Override
+	public void initialize(Date constraintAnnotation) {
+		this.pattern = constraintAnnotation.pattern();
+	}
+
+	@Override
+	public boolean isValid(String input, ConstraintValidatorContext constraintValidatorContext) {
+		if (input == null || input.trim().isEmpty()) {
+			return false;
+		}
+
+		try {
+			LocalDate.parse(input, DateTimeFormatter.ofPattern(this.pattern));
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+}
