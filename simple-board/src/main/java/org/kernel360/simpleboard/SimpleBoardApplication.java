9d7a925a05f51cd78f77cd89028489f756b05352
Photogrammer <khsj15@khu.ac.kr>
2024-07-14 14:56:15 +0900

    init: Create simple-board project

---
+++ simple-board/src/main/java/org/kernel360/simpleboard/SimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package org.kernel360.simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoardApplication.class, args);
+	}
+
+}
---
+++ simple-board/src/main/resources/schema.sql
@@ -0,0 +1,48 @@
+-- -----------------------------------------------------
+-- Table `simple-board`.`board`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `simple_board`.`board`
+(
+    `id`         BIGINT(32)   NOT NULL AUTO_INCREMENT,
+    `board_name` VARCHAR(100) NOT NULL,
+    `status`     VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `simple-board`.`post`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `simple_board`.`post`
+(
+    `id`        BIGINT(32)   NOT NULL AUTO_INCREMENT,
+    `board_id`  BIGINT(32)   NOT NULL,
+    `user_name` VARCHAR(50)  NOT NULL,
+    `password`  VARCHAR(4)   NOT NULL,
+    `email`     VARCHAR(100) NOT NULL,
+    `status`    VARCHAR(50)  NOT NULL,
+    `title`     VARCHAR(100) NOT NULL,
+    `content`   TEXT         NOT NULL,
+    `posted_at` DATETIME     NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `simple-board`.`reply`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `simple_board`.`reply`
+(
+    `id`         BIGINT(32)   NOT NULL AUTO_INCREMENT,
+    `post_id`    BIGINT(32)   NOT NULL,
+    `user_name`  VARCHAR(50)  NOT NULL,
+    `password`   VARCHAR(4)   NOT NULL,
+    `status`     VARCHAR(50)  NOT NULL,
+    `title`      VARCHAR(100) NOT NULL,
+    `content`    TEXT         NOT NULL,
+    `replied_at` DATETIME     NOT NULL,
+    PRIMARY KEY (`id`)
+)
+    ENGINE = InnoDB;
